ce3c2985a146602fd05b6a153f1d169fbf20c040
Jawidegan <jawiqabooban@gmail>
2025-04-04 17:47:53 +0300

    Added new files to the project

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -4,7 +4,7 @@ from django.conf.urls.static import static
 from django.conf import settings
 from django.urls import path
 from . import views
-from .views import logout_view
+from .views import logout_view, registration
 
 app_name = 'djangoapp'
 urlpatterns = [
@@ -14,7 +14,9 @@ urlpatterns = [
     # path for login
     # path(route='login', view=views.login_user, name='login'),
     path(route='login', view=views.login_user, name='login'),
-    path('logout/', logout_view, name='logout'),
+    # path(route='registration', view=views.registration, name='registration'),
+    path('register', registration, name='register'),
+    path('logout', logout_view, name='logout'),
     # path for dealer reviews view
 
     # path for add a review view
